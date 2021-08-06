5016d4efac23e7b53d30613a1e5df0c3e0d20647
Jason S. Robinson <jason.robinson@mayberryinv.com>
2021-08-06 17:47:25 -0500

    Generalize LAST into a property

--- tests/pbt-test.lisp
+++ tests/pbt-test.lisp
@@ -13,6 +13,13 @@
 	(is= (test-bed:biggest x)
 	     (model-biggest x))))
 
+    (named "picks the last number"
+      (for-all ((lis (a-list an-integer))
+		(known-num an-integer))
+	(let* ((known-last (list known-num))
+	       (known-list (append lis known-last)))
+	  (is equal known-last (last known-list)))))
+
     ))
 
 ;;; HELPERS / MODELS
