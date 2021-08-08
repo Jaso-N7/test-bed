792ccbd343ab2e41274fe58f53b422a0595db176
Jason S. Robinson <jason.robinson@mayberryinv.com>
2021-08-08 14:50:51 -0500

    Tests the ordered pairs property regardless of sort order

--- tests/pbt-test.lisp
+++ tests/pbt-test.lisp
@@ -23,7 +23,27 @@
 
     (named "a sorted list has ordered pairs"
       (for-all ((lis (a-list an-integer)))
-	(is ordered (sort lis #'<))))
+	(let ((fn (random-choice #'< #'>)))
+	  (test (ordered (sort lis fn) :test fn)))))
+
+    (named "a sorted list keeps its size"
+      (for-all ((l (a-list an-integer)))
+	(is= (length l) (length (sort l #'<)))))
+
+    (named "no element added"
+      (for-all ((l (a-list an-integer)))
+	(let ((sorted (sort l #'<)))
+	  (mapc #'(lambda (item)
+		    (is member item l))
+		sorted))))
+
+    (named "no element deleted"
+      (for-all ((l (a-list an-integer)))
+	(let ((sorted (sort l #'<)))
+	  (mapc #'(lambda (item)
+		    (is member item sorted))
+		l))))
+
 
     ))
 
@@ -32,8 +52,26 @@
 (defun model-biggest (lis)
   (car (last (sort lis #'<))))
 
-(defun ordered (lis)
-  (if (< (length lis) 2)
-      T
-      (and (<= (car lis) (cadr lis))
-	   (ordered (cdr lis)))))
+(defun ordered (lis &key (test #'<))
+  "Checks if each pair of a list are ordered according to the ordering function TEST.
+Defaults to an ascending ordering."
+  (check-type lis list " >> Only accepts LISTS. ")
+  (check-type test function " >> :TEST supplied MUST be either #'> or #'<.")
+
+  (cond ((< (length lis) 2)
+	 T)
+        ((eq #'< test)
+	 (and (<= (car lis) (cadr lis))
+	      (ordered (cdr lis))))
+	((eq #'> test)
+	 (and (>= (car lis) (cadr lis))
+	      (ordered (cdr lis) :test test)))))
+
+
+(defmacro random-choice (&rest exprs)
+  "Courtesy of Paul Graham's ANSI CL Macros pg 170."
+  `(case (random, (length exprs))
+     ,@(let ((key -1))
+	 (mapcar #'(lambda (expr)
+		     `(,(incf key) ,expr))
+		 exprs))))
