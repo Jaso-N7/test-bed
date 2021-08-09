b8302cfcf4d8ef50e599c2527fc7f9695b9d985b
Jason S. Robinson <jason.robinson@mayberryinv.com>
2021-08-08 21:41:39 -0500

    Created psuedo test suite
    Ability to skip tests out of the suite by adding function names to *SKIPPED* vector,
    or run all by removing names from the *SKIPPED* vector.
    All test names MUST be in *SUITE* regardless of skipped status.

--- tests/tests.lisp
+++ tests/tests.lisp
@@ -1,8 +1,19 @@
 
 (in-package :test-bed/tests)
 
+(defparameter *skipped*
+  (vector #'check-fizzbuzz)
+  "Do not run these tests.")
+
+(defparameter *suite*
+  (vector #'run-units
+	  #'check-fizzbuzz
+	  #'check-biggest)
+  "Test Suite. Holds the names of all the tests to be run.")
+
 (defun check! ()
   "Run ALL tests."
-    (run-units)
-    ;(check-fizzbuzz)
-    (check-biggest))
+  (dotimes (index (length *suite*))
+    (let ((test-name (svref *suite* index)))
+      (unless (find test-name *skipped*)
+	(funcall test-name)))))
